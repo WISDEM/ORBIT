6b7ec420d0b7f778b6daa6f3b95bf9ce43bb428f
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-01-29 11:01:39 -0700

    Cleanup simulation package.

--- ORBIT/simulation/logic/__init__.py
+++ ORBIT/simulation/logic/__init__.py
@@ -4,8 +4,3 @@ __author__ = ["Jake Nunemaker", "Rob Hammond"]
 __copyright__ = "Copyright 2020, National Renewable Energy Laboratory"
 __maintainer__ = ["Jake Nunemaker", "Rob Hammond"]
 __email__ = ["jake.nunemaker@nrel.gov" "robert.hammond@nrel.gov"]
-
-
-from .vessel_logic import (
-    shuttle_items_to_queue,
-)
