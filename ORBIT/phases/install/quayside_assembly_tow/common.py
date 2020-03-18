988258ac9e4310754ac87bdfb775e2287391b88c
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-03-18 16:14:00 -0600

    Added file structure for tow-out installation modules.

---
+++ ORBIT/phases/install/quayside_assembly_tow/common.py
@@ -0,0 +1,7 @@
+"""Common processes and cargo types for quayside assembly and tow-out
+installations"""
+
+__author__ = "Jake Nunemaker"
+__copyright__ = "Copyright 2020, National Renewable Energy Laboratory"
+__maintainer__ = "Jake Nunemaker"
+__email__ = "jake.nunemaker@nrel.gov"
