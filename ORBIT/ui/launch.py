58f488b936838aa1e51f8b2232d21240fb9e8552
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-05-13 15:04:08 -0600

    Added explicit library path to qt windows plugins.

--- ORBIT/ui/launch.py
+++ ORBIT/ui/launch.py
@@ -3,11 +3,16 @@ __copyright__ = "Copyright 2020, National Renewable Energy Laboratory"
 __maintainer__ = "Jake Nunemaker"
 __email__ = ["jake.nunemaker@nrel.gov"]
 
+import os
 import sys
 
+import PySide2
 from PySide2.QtWidgets import QApplication
 
 from ORBIT.ui import App, Config, LoadSave, RunOrbit, LoadWeather, ModuleSelect
+
+qt = os.path.dirname(PySide2.__file__)
+QApplication.addLibraryPath(os.path.join(qt, "plugins"))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
