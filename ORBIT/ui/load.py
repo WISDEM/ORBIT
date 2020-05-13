d3cd99b43a5ab5df815a5b5f06d8f5b16d0f3a21
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-05-13 11:36:07 -0600

    Added basic structure to LoadSave

--- ORBIT/ui/load.py
+++ ORBIT/ui/load.py
@@ -7,8 +7,10 @@ __email__ = ["jake.nunemaker@nrel.gov"]
 from PySide2.QtWidgets import (
     QLabel,
     QWidget,
+    QGroupBox,
     QLineEdit,
     QHBoxLayout,
+    QPushButton,
     QVBoxLayout,
 )
 
@@ -26,7 +28,33 @@ class LoadSave(QWidget):
     def initUI(self):
         """Initializes the UI."""
 
-        hbox = QHBoxLayout()
-        hbox.addWidget(QLabel("Filepath:"))
-        hbox.addWidget(QLineEdit())
-        self.setLayout(hbox)
+        vbox = QVBoxLayout()
+
+        top = QGroupBox()
+        tLayout = QHBoxLayout()
+        tLayout.addWidget(QLabel("Filepath:"))
+        tLayout.addWidget(QLineEdit())
+        top.setLayout(tLayout)
+
+        bot = QGroupBox()
+        bLayout = QHBoxLayout()
+
+        self.load = QPushButton("Load")
+        self.save = QPushButton("Save")
+
+        bLayout.addWidget(self.load)
+        bLayout.addWidget(self.save)
+        bot.setLayout(bLayout)
+
+        vbox.addWidget(top)
+        vbox.addWidget(bot)
+
+        self.setLayout(vbox)
+
+    def onLoad(self):
+        """"""
+        pass
+
+    def onSave(self):
+        """"""
+        pass
