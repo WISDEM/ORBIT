0b2fa24f989087fdb55f82f8a5556b5a0f2a214f
Sophie Bredenkamp <sophie.bredenkamp@nrel.gov>
2021-07-19 17:31:37 -0600

    substation scaffolding updates pt 2

--- ORBIT/phases/design/electrical_export.py
+++ ORBIT/phases/design/electrical_export.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright 2020, National Renewable Energy Laboratory"
 __maintainer__ = ""
 __email__ = []
 
+import numpy as np
 
 from ORBIT.core.library import extract_library_specs
 from ORBIT.phases.design import DesignPhase
@@ -39,13 +40,23 @@ class ElectricalDesign(DesignPhase):
         #     "length": "m",
         #     "unit_cost": "USD",
         # },
-        # "export_system": {
-        #     "cable": {
-        #         "linear_density": "t/km",
-        #         "sections": [("length, km", "speed, km/h (optional)")],
-        #         "number": "int (optional)",
-        #     },
+        
+        # "offshore_substation_electrical":{
+        #     "switchgear": "int",
+        #     "compensation": "int",
+        #     "transformer": "int",
+            
+            
         # },
+        
+        "export_system": {
+            "cable": {
+                "linear_density": "t/km",
+                "sections": [("length, km", "speed, km/h (optional)")],
+                "number": "int (optional)",
+                "diameter": "int",
+            },
+        },
     }
 
     def __init__(self, config, **kwargs):
@@ -54,49 +65,73 @@ class ElectricalDesign(DesignPhase):
         config = self.initialize_library(config, **kwargs)
         self.config = self.validate_config(config)
 
-        self._outputs = {}
+        self._plant_capacity = self.config["plant"]["capacity"]
+        # self.max_cable_capacity = 181.5 # MW , 220kV * 825A
         self._export_design = self.config["export_system_design"]
         self.initialize_cables()
 
     def run(self):
         """Main run function."""
 
-        self.example_computation()
+        self.initialize_cables()
+        self.cable = self.cables[[*self.cables][0]]
+        self.compute_number_cables()
+        self.compute_cable_diameter()
+        self.compute_cable_length()
+        
+    def compute_number_cables(self):
+        """
+        Calculate the total number of required and redundant cables to
+        transmit power to the onshore interconnection.
+        """
 
-    def example_computation(self):
-        """Example computation method."""
+        num_required = np.ceil(self._plant_capacity / self.cable.cable_power)
+        num_redundant = self._design.get("num_redundant", 0)
 
-        depth = self.config["site"]["depth"]
-        var2 = self.config.get("optional_input", "default")
+        self.num_cables = int(num_required + num_redundant)
+        
+    def compute_cable_current(self):
+        """Computes cable diameter."""
+        
+        cable_cap = self._plant_capacity / self.num_cables
+        self.cable_current = cable_cap / self.cable.rated_voltage  # cable_voltage
+        
+        # Using line of best fit
+        # self.cable_diameter = 36.958 * np.exp(0.004 * cable_current)
+     
+    def compute_cable_length(self):
+        """
+        Calculates the total distance an export cable must travel.
+        """
 
-        self.result = var1 + var2
-        self._outputs["example_output"] = self.result
+        added_length = 1.0 + self._design.get("percent_added_length", 0.0)
+        self.length = round(
+            (
+                self.free_cable_length
+                + (self._distance_to_landfall - self.touchdown / 1000)
+                + self._distance_to_interconnection
+            )
+            * added_length,
+            10,
+        )
         
-    def number_cables(self):
-        """Returns number of export cables."""
+    def compute_oss_electrical_components(self):
+        """"Computes number of offshore substation electrical components."""
         
-        # max_cap = 181.5 MW  **220kV * 825A**
-        # project_cap = self.config[""]
-        # 
-        # num_cables = ceiling(project_cap/max_cap)
-        #
-        #
+        self.num_switchgear = self.num_cables * 2
         
-    def cable_current(self):
-        """Returns cabel current rating."""
-        
-        # cable_cap = project_cap / num_cables
-        # cable_current = cable_cap / cable_voltage
-        # 
-        # current -> diameter
-        #
-        #
+        # dependent on plant capacity
+        self.num_transformers = int
+        self.num_compensation = int
+
 
     def initialize_cables(self):
 
         self.cable = extract_library_specs(
             "cables", self._export_design["cables"]
         )
+        
+    
 
     @property
     def detailed_output(self):
