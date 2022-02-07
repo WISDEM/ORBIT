3a845dd74993fd339ce9c06780a53ce3c5d0ab40
jnunemak <jake.nunemaker@nrel.gov>
2022-02-07 16:30:04 -0700

    Modified jacket process times and constraints.

--- ORBIT/phases/install/jacket_install/common.py
+++ ORBIT/phases/install/jacket_install/common.py
@@ -4,7 +4,7 @@ __maintainer__ = "Jake Nunemaker"
 __email__ = "jake.nunemaker@nrel.gov"
 
 
-from marmot import process
+from marmot import false, process
 
 from ORBIT.core import Cargo
 from ORBIT.core.defaults import process_times as pt
@@ -54,9 +54,37 @@ def install_jacket(vessel, jacket, **kwargs):
 
     reequip_time = vessel.crane.reequip(**kwargs)
     # TODO:
+
+    pile_time = kwargs.get("drive_piles_time", 6)
+    for i in range(jacket.num_legs):
+        yield vessel.task_wrapper(
+            "Install Pile",
+            6,
+            constraints={**vessel.operational_limits},
+            **kwargs,
+        )
+
+        yield vessel.task_wrapper(
+            "Drive Pile",
+            pile_time,
+            constraints={**vessel.operational_limits, "night": false()},
+            suspendable=True,
+            **kwargs,
+        )
+
+        if i < (jacket.num_legs - 1):
+            yield vessel.task_wrapper(
+                "Move Between Piles",
+                4,
+                constraints=vessel.transit_limits,
+                suspendable=True,
+                **kwargs,
+            )
+
     yield vessel.task_wrapper(
         "Lift Jacket", 4, constraints=vessel.operational_limits, **kwargs
     )
+
     yield vessel.task_wrapper(
         "Lower and Position Jacket",
         8,
@@ -64,11 +92,6 @@ def install_jacket(vessel, jacket, **kwargs):
         **kwargs,
     )
 
-    pile_time = kwargs.get("drive_piles_time", 3)
-    for _ in range(jacket.num_legs):
-        yield vessel.task_wrapper(
-            "Drive Pile",
-            pile_time,
-            constraints=vessel.operational_limits,
-            **kwargs,
-        )
+    yield vessel.task_wrapper(
+        "Grout Jacket", 8, constraints=vessel.transit_limits, **kwargs
+    )
