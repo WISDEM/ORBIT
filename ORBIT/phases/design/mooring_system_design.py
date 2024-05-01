671bd2a6295d79dcb04fd726a91bafdd0d719b0f
nriccobo <Nicholas.Riccobono@nrel.gov>
2024-05-01 09:26:10 -0600

    Added anchor cost and total line cost of semitaut option.

--- ORBIT/phases/design/mooring_system_design.py
+++ ORBIT/phases/design/mooring_system_design.py
@@ -57,6 +57,7 @@ class MooringSystemDesign(DesignPhase):
         config = self.initialize_library(config, **kwargs)
         self.config = self.validate_config(config)
         self.num_turbines = self.config["plant"]["num_turbines"]
+        self.depth = self.config["site"]["depth"]
 
         self._design = self.config.get("mooring_system_design", {})
         self.num_lines = self._design.get("num_lines", 4)
@@ -66,15 +67,21 @@ class MooringSystemDesign(DesignPhase):
         # Input hybrid mooring system design from Cooperman et al. (2022),
         # https://www.nrel.gov/docs/fy22osti/82341.pdf
         _semitaut = {
-            "depths": np.array([500, 750, 1000, 1250, 1500], dtype=float),
+            "depths": np.array([500.0, 750.0, 1000.0, 1250.0, 1500.0]),
             "rope_lengths": np.array(
-                [478.41, 830.34, 1229.98, 1183.93, 1079.62], dtype=float
+                [478.41, 830.34, 1229.98, 1183.93, 1079.62]
             ),
-            "rope_diameters": np.array([0.2, 0.2, 0.2, 0.2, 0.2], dtype=float),
+            "rope_diameters": np.array([0.2, 0.2, 0.2, 0.2, 0.2]),
             "chain_lengths": np.array(
-                [917.11, 800.36, 609.07, 896.42, 1280.57], dtype=float
+                [917.11, 800.36, 609.07, 896.42, 1280.57]
             ),
             "chain_diamters": np.array([0.13, 0.17, 0.22, 0.22, 0.22]),
+            "anchor_costs": np.array(
+                [112766.0, 125511.0, 148703.0, 204988.0, 246655.0]
+            ),
+            "total_line_costs": np.array(
+                [826598.0, 1221471.0, 1682208.0, 2380035.0, 3229700.0]
+            ),
         }
 
         self.finterp_rope_l = interp1d(
@@ -88,6 +95,12 @@ class MooringSystemDesign(DesignPhase):
         )
         self.finterp_chain_d = interp1d(
             _semitaut["depths"], _semitaut["chain_diameters"]
+        )
+        self.finterp_anchor_cost = interp1d(
+            _semitaut["depths", _semitaut["anchor_costs"]]
+        )
+        self.finterp_total_line_cost = interp1d(
+            _semitaut["depths"], _semitaut["total_line_costs"]
         )
 
         self._outputs = {}
@@ -140,7 +153,6 @@ class MooringSystemDesign(DesignPhase):
         """
         Returns the mooring line length and mass.
         """
-        depth = self.config["site"]["depth"]
 
         if self.mooring_type == "Catenary":
             if self.anchor_type == "Drag Embedment":
@@ -150,7 +162,10 @@ class MooringSystemDesign(DesignPhase):
                 fixed = 0
 
             self.line_length = (
-                0.0002 * (depth**2) + 1.264 * depth + 47.776 + fixed
+                0.0002 * (self.depth**2)
+                + 1.264 * self.depth
+                + 47.776
+                + fixed
             )
 
             self.line_mass = self.line_length * self.line_mass_per_m
@@ -163,11 +178,11 @@ class MooringSystemDesign(DesignPhase):
                 fixed = 0
 
             # Rope and chain length at project depth
-            self.chain_length = self.finterp_chain_l(depth)
-            self.rope_length = self.finterp_rope_l(depth)
+            self.chain_length = self.finterp_chain_l(self.depth)
+            self.rope_length = self.finterp_rope_l(self.depth)
             # Rope and chain diameter at project depth
-            self.rope_diameter = self.finterp_rope_d(depth)
-            self.chain_diameter = self.finterp_chain_d(depth)
+            self.rope_diameter = self.finterp_rope_d(self.depth)
+            self.chain_diameter = self.finterp_chain_d(self.depth)
 
             self.line_length = self.rope_length + self.chain_length
 
@@ -193,19 +208,36 @@ class MooringSystemDesign(DesignPhase):
         review. Should be revised when this module is overhauled in the future.
         """
 
-        if self.anchor_type == "Drag Embedment":
-            self.anchor_mass = 20
-            self.anchor_cost = self.breaking_load / 9.81 / 20.0 * 2000.0
+        if self.mooring_type == "Catenary":
+            if self.anchor_type == "Drag Embedment":
+                self.anchor_mass = 20
+                self.anchor_cost = self.breaking_load / 9.81 / 20.0 * 2000.0
 
+            else:
+                self.anchor_mass = 50
+                self.anchor_cost = (
+                    sqrt(self.breaking_load / 9.81 / 1250) * 150000
+                )
         else:
-            self.anchor_mass = 50
-            self.anchor_cost = sqrt(self.breaking_load / 9.81 / 1250) * 150000
+            if self.anchor_type == "Drag Embedment":
+                self.anchor_mass = 20
+
+            else:
+                self.anchor_mass = 50
+
+            self.anchor_cost = self.finterp_anchor_cost(self.depth)
 
     @property
     def line_cost(self):
         """Returns cost of one line mooring line."""
 
-        return self.line_length * self.line_cost_rate
+        if self.mooring_type == "Catenary":
+            line_cost = self.line_length * self.line_cost_rate
+
+        else:
+            line_cost = self.finterp_total_line_cost(self.depth)
+
+        return line_cost
 
     @property
     def total_cost(self):
@@ -214,7 +246,7 @@ class MooringSystemDesign(DesignPhase):
         return (
             self.num_lines
             * self.num_turbines
-            * (self.anchor_cost + self.line_length * self.line_cost_rate)
+            * (self.anchor_cost + self.line_cost)
         )
 
     @property
